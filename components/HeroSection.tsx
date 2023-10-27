6d4f82a93a21b5aa293a738e8c35b6030ba0c4c6
Ladeece1 <106746098+Ladeece1@users.noreply.github.com>
2023-10-27 22:02:05 +0800

    Update HeroSection.tsx

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -28,7 +28,7 @@ const HeroSection = () => {
             based in Kuala Lumpur, Malaysia. 
           </p>
           <a
-            href="/LimEnXuanResume.pdf"
+            href="/MyNewestResume.pdf"
             className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700 cursor-pointer"
           >
             My Resume
